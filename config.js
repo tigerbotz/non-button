7a63501c1cb9d9d96ece31b1d117a15215d8a467
Rush2007 <144612802+Rush2007@users.noreply.github.com>
2024-08-24 07:38:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/a7ccc1537d4760ce7a0cc.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello!* . 'This is Thenu-MD Whatsapp WA BOT'...I'm alive now.. ..♻️😏..",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2d4d67f5088957f82b558.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "🇭​​🇮​ , ​🇦​​🇲​ ​🇦​​🇱​​🇮​​🇻​​🇪​ ​🇳​​🇴​​🇼​ ​🇹​​🇭​​🇮​​🇸​  ​🇮​​🇸​ ​🇹​​🇮​​🇬​​🇪​​🇷​ ​🇸​​🇹​​🇦​​🇹​​🇺​​🇸​ ​🇲​​🇩​.. ..🥷㊙️...",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public"
 
