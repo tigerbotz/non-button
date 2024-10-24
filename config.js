0fc4251d32fe4cbda3b13d07b2543e436baac9db
WHITE_TIGER_BOTZ_INC <imsehpasidu@gmail.com>
2024-10-24 20:03:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2d4d67f5088957f82b558.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "🇭​​🇮​ , ​🇦​​🇲​ ​🇦​​🇱​​🇮​​🇻​​🇪​ ​🇳​​🇴​​🇼​ ​🇹​​🇭​​🇮​​🇸​  ​🇮​​🇸​ ​🇹​​🇮​​🇬​​🇪​​🇷​ ​🇸​​🇹​​🇦​​🇹​​🇺​​🇸​ ​🇲​​🇩​.. ..🥷㊙️...",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public"
-
+SESSION_ID: process.env.SESSION_ID || "HHRACSgL#KMc5qcyffm_q-3CMLagiA0I-MW51N66D88VHrQLUg34",
+MONGODB: process.env.MONGODB || "mongodb+srv://shanuawatech:@-Render-88@cluster0.he454.mongodb.net/",//enter mongo db url
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/iuwjg1.jpg"
+ALIVE-MSG: process.env.ALIVE_MSG || "Hello im alive now"
 };
